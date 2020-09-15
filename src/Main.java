328730a2ea337fbbb43339b0db1b3a9296b9f2f9
Alen Elzner <indigokris44@gmail.com>
2020-09-15 22:02:08 +0300

    Added testability java code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 2090;  //цена с копейками
-        int coefficient = 20;
-        int bonus = ticket_price / coefficient / 100;
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
